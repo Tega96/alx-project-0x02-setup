0e5fdd1290cd111d8c28c8cc443b8dfdc01a9c73
Otega Otite <otiteotega@gmail.com>
2025-11-20 17:12:05 +0100

    Create a reusable card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,3 +1,4 @@
+import Card from "@/components/common/Card";
 import Header from "@/components/layout/Header";
 
 
@@ -5,7 +6,10 @@ const Home = () => {
     return (
         <div>
             <Header />
-            <h2>Hello Home page...</h2>
+            <Card 
+                title="Benefits of sitting down to read"
+                content="Sitting down to read has an enormous benefits than the mouth can tell. I will patiently tell you those quite soon."
+            />
         </div>
     )
 }
